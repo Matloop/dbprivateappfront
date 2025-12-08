a48c85528e7cd8838847e0766dbf78d2b7653da9
Matloop <matheusdiasestacioo@gmail.com>
2025-12-08 10:19:23 -0300

    supabase key in .env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = 'https://zplgyjoakzxehysmerds.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwbGd5am9ha3p4ZWh5c21lcmRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjQwMjcsImV4cCI6MjA3OTc0MDAyN30.T-TpYa2Qu_DmXsldKAIQ2twRJXeKCOxwhCuJml6oKW8'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL 
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("As variáveis de ambiente do Supabase não estão definidas.")
+}
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
